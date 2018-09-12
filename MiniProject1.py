b507c2331da21e52bf14dccfdcf726ffe21aa411
SeanNonnemacher <seann@bu.edu>
2018-09-12 12:52:08 -0400

    Added getImages and analyzeVIdeo

--- MiniProject1.py
+++ MiniProject1.py
@@ -1,6 +1,12 @@
 import CreateVideo as createVideo
+import AnalyzeVideo as analyzeVideo
+import GetPictures as getPictures
 
 TWITTER = "test"
 FRAME_DURATION = ".25"
+
+getPictures.getPictures(TWITTER)
     
 createVideo.createVideo(TWITTER, FRAME_DURATION)
+
+analyzeVideo.analyzeVideo(TWITTER)
