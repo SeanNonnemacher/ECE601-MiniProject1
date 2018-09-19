3957d2dbc37971f45e625a38552a3eb1e7dcfb8f
SeanNonnemacher <seann@bu.edu>
2018-09-18 20:24:27 -0400

    Added comments and renamed variables

--- AnalyzeVideo.py
+++ AnalyzeVideo.py
@@ -3,31 +3,34 @@ import io
 
 from google.cloud import videointelligence
 
+#location of Google Credentials.json file
+GOOGLE_KEY_JSON_FILE = "C:/Users/Sean/Downloads/key.json"
 
+#Analyzes video from given location
 def analyzeVideo(username):
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r"C:\Users\Sean\Downloads\key.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_KEY_JSON_FILE
     print("Videos/" + username + ".mp4")
+    
     with io.open("Videos/" + username + ".mp4", 'rb') as movie:
         input_content = movie.read()
-        
-    analyze_labels(input_content)
+    analyzeLabels(input_content)
 
 
 
 
-
-def analyze_labels(input_content):
-    video_client = videointelligence.VideoIntelligenceServiceClient()
+#Analyzes video and outputs labels to console
+def analyzeLabels(input_content):
+    videoClient = videointelligence.VideoIntelligenceServiceClient()
     features = [videointelligence.enums.Feature.LABEL_DETECTION]
-    operation = video_client.annotate_video(input_content = input_content, features=features)
+    operation = videoClient.annotate_video(input_content = input_content, features=features)
    
     print('\nProcessing video for label annotations:')
 
     result = operation.result(timeout=90)
     print('\nFinished processing.')
 
-    shot_labels = result.annotation_results[0].shot_label_annotations
-    for i, shot_label in enumerate(shot_labels):
+    shotLabels = result.annotation_results[0].shot_label_annotations
+    for i, shot_label in enumerate(shotLabels):
         print('Shot label description: {}'.format(
             shot_label.entity.description))
         for category_entity in shot_label.category_entities:
@@ -35,11 +38,11 @@ def analyze_labels(input_content):
                 category_entity.description))
     
         for i, shot in enumerate(shot_label.segments):
-            start_time = (shot.segment.start_time_offset.seconds +
+            startTime = (shot.segment.start_time_offset.seconds +
                           shot.segment.start_time_offset.nanos / 1e9)
-            end_time = (shot.segment.end_time_offset.seconds +
+            endTime = (shot.segment.end_time_offset.seconds +
                         shot.segment.end_time_offset.nanos / 1e9)
-            positions = '{}s to {}s'.format(start_time, end_time)
+            positions = '{}s to {}s'.format(startTime, endTime)
             confidence = shot.confidence
             print('\tSegment {}: {}'.format(i, positions))
             print('\tConfidence: {}'.format(confidence))
