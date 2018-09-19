3e2aa1c5267ea2cc770696fbfcb04114159050cb
SeanNonnemacher <seann@bu.edu>
2018-09-18 20:24:59 -0400

    Added creation of folder for videos if it does not exist

--- CreateVideo.py
+++ CreateVideo.py
@@ -1,9 +1,20 @@
 import os
 
+#Creates video from given folder of images
 def createVideo(username, frameDuration):
+    videoFolder = "Videos"
+    createFolder(videoFolder)
+    
     try:
         command = "ffmpeg -r "+frameDuration+" -s 1920x1080 -i "+username+"Images/"+username+"%04d.jpg -vcodec mpeg4 -y Videos/"+username+".mp4"
         os.system(command)
     except:
         print("FFmpeg video creation error")
         
+#Creates folder for videos if it does not exist
+def createFolder(folder):
+    try:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+    except:
+        print("Video folder not successfully created")
