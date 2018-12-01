18556a5309ccf545cb9d7b6e5e49d0561fe8c414
SeanNonnemacher <seann@bu.edu>
2018-11-30 21:27:39 -0500

    Added entering data into database

--- MiniProject1.py
+++ MiniProject1.py
@@ -1,14 +1,30 @@
 import CreateVideo as createVideo
 import AnalyzeVideo as analyzeVideo
 import GetPictures as getPictures
+from TwitterDBConnector import MyDB
+
 
 #Changeable constants
 USERNAME = "NASA"
 NUM_IMAGES = 10
-FRAME_DURATION = ".25"
+FRAME_DURATION = "2"
 
-getPictures.getPictures(USERNAME, NUM_IMAGES)
+
+
+try:
+    myDB = MyDB()
     
-createVideo.createVideo(USERNAME, FRAME_DURATION)
-
-analyzeVideo.analyzeVideo(USERNAME)
+    getPictures.getPictures(USERNAME, NUM_IMAGES)
+    
+    createVideo.createVideo(USERNAME, FRAME_DURATION)
+    
+    labels = analyzeVideo.analyzeVideo(USERNAME)
+    
+    sessionID = myDB.add_new_session(USERNAME, NUM_IMAGES, len(labels))
+    
+    for label in labels:
+        myDB.add_new_label(sessionID, label)
+except Exception as e:
+    print(e)
+    
+myDB.close_connection()
